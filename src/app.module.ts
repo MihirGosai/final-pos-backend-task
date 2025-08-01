fb3fec1ca033defbadf13c587c5eeb39264c2e5e
Mihir Gosai <mihirgosai@live.com>
2025-08-01 12:43:48 -0230

    feat(cart): init commit for cart module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/final-pos'),
     ProductModule,
+    CartModule,
   ],
 })
 export class AppModule {}
