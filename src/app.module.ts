d5d162e42d7e92e8dd2ce1587e6423d107374e46
Mihir Gosai <mihirgosai@live.com>
2025-08-07 15:46:59 -0230

    chore(docker): add Docker support and environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/final-pos'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
     ProductModule,
     CartModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log('🚀 Server is running on http://localhost:3000');
 }
 void bootstrap();
